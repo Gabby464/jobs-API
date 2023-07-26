ee9bd437c5021377b609e681c5fe28b07e527c10
Gabriela P <gabriela.poryazova@gmail.com>
2023-07-26 21:29:18 +0300

    Install security packages

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const dotenv = require("dotenv");
 require("express-async-errors");
 dotenv.config();
+
+//security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
 const express = require("express");
+
 const connectDB = require("./db/connect");
+//routers
 const jobsRouter = require("./routes/jobs");
 const authRouter = require("./routes/auth");
 const app = express();
@@ -19,9 +27,18 @@ app.get("/", (req, res) => {
 app.use("/jobs-api/v1/jobs", jobsRouter);
 app.use("/jobs-api/v1/auth", authRouter);
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
-
+app.use(cors());
+app.use(helmet());
+app.use(limiter);
+app.set('trust proxy', 1)
 const port = process.env.PORT || 3000;
 
 const start = async () => {
